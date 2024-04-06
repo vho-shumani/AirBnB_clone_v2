5ae5fcef9b6aba6b365d891db7e06420faf09c3c
Shumani Simeli <shumanisimeli@gmail.com>
2024-04-07 00:36:32 +0200

    updated 2-do_deploy_web_static.py
    mend

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,11 +17,13 @@ def do_deploy(archive_path):
         put(archive_path, "/tmp/")
         archive = os.path.basename(archive_path)
         folder = f'/data/web_static/releases/{archive.split(".")[0]}'
-        run(f"mkdir -p {folder}")
-        run(f"tar -xzf /tmp/{archive} -C {folder}")
+        run(f"mkdir -p {folder}/")
+        run(f"tar -xzf /tmp/{archive} -C {folder}/")
         run(f"rm /tmp/{archive}")
+        run(f"mv {folder}/web_static/* {folder}")
+        run(f"rm -rf {folder}/web_static")
         run("rm -rf /data/web_static/current")
-        run(f"ln -s {folder} /data/web_static/current")
+        run(f"ln -s {folder}/ /data/web_static/current")
         return True
     except Exception as e:
         print(e)
