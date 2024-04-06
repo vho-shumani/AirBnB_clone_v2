fc52ae7b2d985653375d0db63aa52e11908c1184
Shumani Simeli <shumanisimeli@gmail.com>
2024-04-07 00:06:51 +0200

    created 2-do_deploy_web_static.py

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Modules contains function that distributes
+an archive to your web servers."""
+from fabric.api import *
+import os
+
+
+env.hosts = ['54.158.176.162', '100.25.199.183']
+
+
+def do_deploy(archive_path):
+    """ distributes an archive to web servers"""
+    if not os.path.exists(archive_path):
+        return False
+
+    try:
+        put(archive_path, "/tmp/")
+        archive = os.path.basename(archive_path)
+        folder = f'/data/web_static/releases/{archive.split('.')[0]}'
+        run(f"mkdir -p {folder}")
+        run(f"tar -xzf /tmp/{archive} -C {folder}")
+        run(f"rm /tmp/{archive}")
+        run("rm -rf /data/web_static/current")
+        run(f"ln -s {folder}/ /data/web_static/current")
+        return True
+    except Exception as e:
+        print(e)
+        return False
