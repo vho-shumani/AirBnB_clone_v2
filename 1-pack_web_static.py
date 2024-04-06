b0c00119dd584240383a11436cbaa911651829a9
Shumani Simeli <shumanisimeli@gmail.com>
2024-04-06 21:15:33 +0200

    updated 1-pack_web_static.py
    mend

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""Generate .tgz archive"""
-from fabric.api import local
+"""module contains the function do_pack that generates a .tgz archive"""
+from fabric.api import *
 from datetime import datetime
+
 
 def do_pack():
     """generates a .tgz archive from the contents of the web_static folder"""
-    time = datetime.now().strftime(%Y%m%d%H%M%S)
+    time = datetime.now().strftime("%Y%m%d%H%M%S")
     local('sudo mkdir -p versions')
     result = local(f'tar -czvf versions/web_static_{str_time}.tgz web_static')
     if result.succeeded:
