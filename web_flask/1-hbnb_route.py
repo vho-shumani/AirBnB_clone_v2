62585d409e2b71e0c592965e111970b311164330
Shumani Simeli <shumanisimeli@gmail.com>
2024-04-20 16:56:05 +0200

    created 1-hbnb_route.py

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Start a flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """
+    Defines a view function for root url.
+    Returns: (str) text message
+    """
+    return "Hello HBNB"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    Defines a view function for /hbnb url.
+    Returns: (str) text message
+    """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
