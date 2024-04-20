a51c9e2e89b7eb052f4757caff05131c29f309fd
Shumani Simeli <shumanisimeli@gmail.com>
2024-04-20 16:30:58 +0200

    first commit

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module starts flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+    Define a view function for the root url
+    
+    Returns: (str) a greeting message.
+    """
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
