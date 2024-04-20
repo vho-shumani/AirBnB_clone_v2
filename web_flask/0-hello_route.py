4146679018d4e4d2ec670c6a69376ee584b6be7f
Shumani Simeli <shumanisimeli@gmail.com>
2024-04-20 16:35:01 +0200

    updated 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,14 +4,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     """
     Define a view function for the root url
-    
     Returns: (str) a greeting message.
     """
     return "Hello HBNB!"
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
